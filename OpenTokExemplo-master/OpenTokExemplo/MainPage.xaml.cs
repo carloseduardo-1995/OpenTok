91657d5aac7cb9569cba235b4d764841747266a5
Carlos Eduardo M Junior <carlos.junior@mobrj.com.br>
2020-04-30 12:26:40 -0300

    Acesso para conversa de 1 pessoa

--- OpenTokExemplo-master/OpenTokExemplo/MainPage.xaml.cs
+++ OpenTokExemplo-master/OpenTokExemplo/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,15 +14,33 @@ namespace OpenTokExemplo
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage,INotifyPropertyChanged
     {
-        
+        private bool _abreBarra = false;
+        public bool AbreBarra
+        {
+            get { return _abreBarra; }
+            set {
+                _abreBarra = value;
+                OnPropertyChanged("AbreBarra");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+       
         public MainPage()
         {
             InitializeComponent();
+            
             NavigationPage.SetHasBackButton(this, false);
         }
 
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         private void OnEndCall(object sender, EventArgs e)
         {
@@ -41,6 +60,13 @@ namespace OpenTokExemplo
         private void OnMicrofone(object sender, EventArgs e)
         {
             CrossOpenTok.Current.IsAudioPublishingEnabled = !CrossOpenTok.Current.IsAudioPublishingEnabled;
+        }
+        private void OnAbreMenu(object sender, EventArgs e)
+        {
+            _abreBarra = !_abreBarra;
+            Botoes.IsEnabled = AbreBarra;
+            Botoes.IsVisible = AbreBarra;
+            Debug.WriteLine(_abreBarra.ToString());
         }
         
     }
